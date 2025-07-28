09bf9b42da1446cd487dc1b565277c52eaff11b5
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2025-07-28 18:09:27 +0100

    Initial commit

---
+++ src/main/java/com/dailycodework/fisrtprojectspringboot/FisrtProjectSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodework.fisrtprojectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisrtProjectSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisrtProjectSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailycodework/fisrtprojectspringboot/FisrtProjectSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodework.fisrtprojectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisrtProjectSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
