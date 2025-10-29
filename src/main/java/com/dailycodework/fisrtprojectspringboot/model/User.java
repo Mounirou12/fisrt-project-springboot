c446de6f92fa028abb5fca9e9c2a086a98240bc8
FAGBEMY MOUNIROU <fagbemymounirou@gmail.com>
2025-10-29 20:47:00 +0100

    Adds security support and user-role mapping
    Integrates Spring Security to enable authentication and authorization features.
    Introduces Many-to-Many relationship between users and roles to support flexible role assignment.
    Lays groundwork for access control and user management enhancements.

--- src/main/java/com/dailycodework/fisrtprojectspringboot/model/User.java
+++ src/main/java/com/dailycodework/fisrtprojectspringboot/model/User.java
@@ -1,14 +1,20 @@
 package com.dailycodework.fisrtprojectspringboot.model;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 import org.hibernate.annotations.NaturalId;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
@@ -39,4 +45,11 @@ public class User {
 
     @OneToMany(mappedBy="user",cascade=CascadeType.ALL,orphanRemoval=true)// Relation One-to-Many : une seule instance de User peut avoir plusieurs instances de Order
     private List<Order> orders;
+
+    @ManyToMany(fetch=FetchType.EAGER,cascade= // Relation Many-to-Many : une seule instance de User peut avoir plusieurs instances de Role
+    {CascadeType.DETACH, CascadeType.MERGE ,CascadeType.PERSIST,CascadeType.REFRESH}) // les opérations sur User affecteront ces entités roles  
+    @JoinTable(name="user_roles", joinColumns= @JoinColumn(name="user_id", referencedColumnName="id"),// Spécifie la table intermédiaire pour la relation Many-to-Many 
+    inverseJoinColumns=@JoinColumn(name="role_id", referencedColumnName="id")// Spécifie la colonne de jointure inverse pour la relation Many-to-Many
+    )
+    private  Collection<Role> roles = new HashSet<>();
 }
